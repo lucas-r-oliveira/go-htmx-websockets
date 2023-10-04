fe42dab84f1ae1145bf66abb51d50a7525bf6ec6
lucas-r-oliveira <lucasoliveira7c@gmail.com>
2023-10-04 12:45:57 +0100

    message send and append

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 )
+
+var templates = template.Must(template.ParseGlob("tmpl/*.html"))
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	err := templates.ExecuteTemplate(w, "index.html", nil)
@@ -14,11 +17,17 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-var templates = template.Must(template.ParseGlob("tmpl/*.html"))
+func newMessageHandler(w http.ResponseWriter, r *http.Request) {
+	// the message will come as form data
+	fmt.Printf("Form data: %#v\n", r.FormValue("message"))
+	t, _ := template.New("message").Parse(`<p>{{.}}</p>`)
+	_ = t.Execute(w, r.FormValue("message"))
+}
 
 func main() {
 
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/send-message", newMessageHandler)
 
 	http.ListenAndServe(":8080", nil)
 	log.Fatalln("Shutting down...")
